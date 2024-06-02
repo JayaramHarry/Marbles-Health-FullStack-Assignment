6911a79fab7531c41a72bd4fd55aaeba3bbea0b6
JayaramHarry <121944470+JayaramHarry@users.noreply.github.com>
2024-06-03 03:10:42 +0530

    new changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,19 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/users', userRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB connected');
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
