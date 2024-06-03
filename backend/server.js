3111a06cce61c6213943348b29f1507a80a64f81
JayaramHarry <121944470+JayaramHarry@users.noreply.github.com>
2024-06-03 12:50:52 +0530

    updated mongo server

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,36 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// const userRoutes = require('./routes/userRoutes');
+
+// app.use('/api/users', userRoutes);
+
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => {
+//   console.log('MongoDB connected');
+// })
+// .catch((error) => {
+//   console.error('Error connecting to MongoDB:', error);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,24 +40,27 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-app.use(cors());
+// Define CORS options to allow requests from your Netlify domain
+const corsOptions = {
+  origin: 'https://brilliant-croissant-205f6b.netlify.app', // Replace this with your Netlify domain
+};
+
+// Use CORS middleware with the specified options
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/users', userRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => {
   console.log('MongoDB connected');
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
 })
 .catch((error) => {
   console.error('Error connecting to MongoDB:', error);
